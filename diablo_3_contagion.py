cf53987e632beb8b4839755b3a20a6f55f6139b3
thomtyrrell <thomtyrrell@icloud.com>
2015-10-28 12:42:22 -0400

    Now uses JSON to save data dictionary.  Included method to export some data into a csv file.  Formatting changes.

--- diablo_3_contagion.py
+++ diablo_3_contagion.py
@@ -13,17 +13,58 @@
 # of a mark. 
 
 import collections
+
 import numpy as np
-import scipy as sc
 from numpy import random as rng
+
 import matplotlib.pyplot as plt
+import json
+import time
+
+from scipy.misc import comb
+from math import gamma, exp, factorial
+from statistics import mean
+
+
+# probability methods
+
+
+def exp_spread(p, m):
+    return p ** m
+
+
+def prob_x(n, s, p, prob_spread=exp_spread):
+    prob = prob_spread(p, (n ** 2 + n) / 2)
+    return comb(3 ** n, s, exact=True) * prob ** s * (1 - prob) ** (3 ** n - s) * 1.0
+
+
+# exponential spread methods
+
+
+def expect_x(n, p):
+    return 3 ** n * p ** ((n ** 2 + n) / 2)
+
+
+def prob_s(n, p):
+    return (prob_x(n, 0, p) - prob_x(n - 1, 0, p)) / (1 - prob_x(n - 1, 0, p))
+
+
+def expect(p, n_bar=150):
+    return sum([3 ** n * p ** ((n ** 2 + n) / 2) for n in range(n_bar)])
+
+
+def var(p, n_bar=150):
+    return sum([3 ** n * p ** ((n ** 2 + n) / 2) * (1 - p ** ((n ** 2 + n) / 2)) for n in range(n_bar)])
+
+
+#
+# simulation methods
+#
 
 # Given such a counter marks and initial probability of spreading p,
 # kill_enemy(...) randomly chooses an enemy to kill, and spreads the mark
 # probabilistically.  We use numpy.random for random number generation.
 # Currently randomly chooses an enemy to kill
-
-def exp_spread(p,m): return p ** m
 
 # sim_contagion(...) will simulate an outbreak and count the number of spreads.
 def sim_contagion(p, prob_spread=exp_spread):
@@ -32,101 +73,172 @@ def sim_contagion(p, prob_spread=exp_spread):
     while marks[n] > 0:  # while there are enemies still alive
         for _ in range(marks[n]):
             roll = rng.random()
-            if roll < prob_spread(p,n):
+            if roll < prob_spread(p, n):
                 marks[n + 1] += 3
         n += 1
-    return sum(np.array(marks.values()) / 3)
+    return marks
 
 
-def batch_simulate(p,prob_spread=exp_spread,N=100):
-    data = {"Cont":[],"Mean":[],"Hist":collections.Counter(),"p":p,"spread":prob_spread,"N":N}
-    for _ in range(1, N):
-        roll_cont = sim_contagion(p,prob_spread)
+def batch_simulate(p, data=None, prob_spread=exp_spread, n=100):
+    if data is None:
+        data = {"Cont": [], "Stop": [], "Hist": collections.Counter(), "StopHist": collections.Counter(), "p": float(p),
+                "spread": str(prob_spread), "n": int(n)}
+    elif p != data["p"] or str(prob_spread) != data["spread"]:
+        print "Error:  Mismatched parameter values"
+        return data
+    else:
+        data["n"] += int(n)
+    for _ in range(1, n + 1):
+        marks = sim_contagion(p, prob_spread)
+        roll_stop = np.max(marks.keys())
+        data["Stop"].append(roll_stop)
+        data["StopHist"][roll_stop] += 1
+        roll_cont = sum(np.array(marks.values()) / 3)
         data["Cont"].append(roll_cont)
-        data["Mean"].append(np.mean(data["Cont"]))
-        data["Hist"][roll_cont]+=1
+        data["Hist"][roll_cont] += 1
     return data
 
 
+def save_dict(data):
+    now = str(int(time.time() * 100))
+    f = open('/Users/thomtyrrell/Documents/Code/GitHub/Diablo-3-Contagion/cont_dict' + now + '.txt', 'a')
+    json.dump(data, f)
+    f.close()
+
+
+def export_csv(data):
+    now = str(int(time.time() * 100))
+    f = open('/Users/thomtyrrell/Documents/Code/GitHub/Diablo-3-Contagion/cont_data' + now + '.csv', 'a')
+    f.write("Contaminations,Depth" + "\n")
+    for i in range(data["N"]):
+        f.write(str(data["Cont"][i]) + ',' + str(data["Stop"][i]) + "\n")
+    f.close()
+
+
+#
+# visualization methods
+#
+
 def visual_scatter(data):
-    N = data["N"]
+    n = data["n"]
     plt.close()
-    plt.scatter(range(1, N), data["Cont"], s=1, color="red", label="Contaminations during trial")
-    plt.plot(range(1, N), data["Mean"], '-', color="purple", label="Mean")
+    plt.scatter(range(1, n + 1), data["Cont"], s=1, color="red", label="Contaminations during trial")
+    plt.plot(range(1, n + 1), [np.mean(data["Cont"])] * n, '-', color="purple", label="Mean")
     plt.xlabel("Trial")
     plt.ylabel("Number of contaminations during trial")
-    plt.xlim(1,N)
-    plt.ylim(1,np.max(sorted(data["Hist"].keys())))
+    plt.xlim(1, n)
+    plt.ylim(1, np.max(sorted(data["Hist"].keys())))
     plt.legend(shadow=True)
-    plt.savefig(str(N) + ' trial graph.png')
-    print "Mean number of contaminations over " + str(N) + " trials is " + str(data["Mean"][-1]*1.)
-    print "Standard Deviation of number of contaminations over " + str(N) + " trials is " + str(np.std(data["Cont"]))
+    plt.savefig(str(n) + ' trial graph.png')
+    print "Mean number of contaminations over " + str(n) + " trials is " + str(np.mean(data["Cont"]))
+    print "Standard Deviation of number of contaminations over " + str(n) + " trials is " + str(np.std(data["Cont"]))
+
+
+def visual_stop(data):
+    n = data["n"]
+    stop_data = data["Stop"]
+    plt.close()
+    plt.plot(range(1, n + 1), [np.mean(stop_data)] * n, '-', color="purple", label="Mean")
+    plt.scatter(range(1, n + 1), stop_data, s=1, color="red")
+    plt.xlabel("Trial")
+    plt.ylabel("Stopping Time")
+    plt.xlim(1, n)
+    plt.ylim(1, np.max(stop_data))
+    plt.legend(shadow=True)
+    plt.savefig(str(n) + ' trial graph.png')
+    print "Mean stopping level over " + str(n) + " trials is " + str(np.mean(stop_data))
 
 
 def visual_hist(data):
-    N = data["N"]
     cont = np.array(data["Cont"])
     keys = sorted(data["Hist"].keys())
     plt.close()
-    plt.hist(cont, bins=[s-.5 for s in keys+[keys[-1]+3]], normed=1, width=1, facecolor='red')
-    plt.xlim(0,np.max(keys))
+    plt.hist(cont, bins=[s - .5 for s in keys + [keys[-1] + 3]], normed=1, width=1, facecolor='red')
+    plt.xlim(0, np.max(keys))
     plt.xlabel("Number of contaminations during trial")
     plt.ylabel("Portion of Trials")
+    plt.savefig('hist.png')
+
+
+def visual_stop_hist(data):
+    stop = np.array(data["Stop"])
+    keys = sorted(data["StopHist"].keys())
+    max_stop = np.max(stop)
+    x = range(1, max_stop + 1)
+    fun = lambda t: prob_s(t, data["p"])
+    y = [fun(n) for n in x]
+    plt.close()
+
+    plt.hist(stop, bins=[s - .5 for s in keys + [keys[-1] + 3]], normed=1, width=1, facecolor='red')
+    plt.plot(x, y, '-', color="purple", label="Stop Distribution")
+
+    plt.xlim(0, max_stop + 1)
+    plt.xlabel("Stopping time during trial")
+    plt.ylabel("Portion of Trials")
+    plt.legend(shadow=True)
     plt.savefig('hist.png')
 
 
 def gamma_density_approx(data):
-    data_mean = data["Mean"][-1]*1.
+    data_mean = np.mean(data["Cont"])
     data_std = np.std(data["Cont"])
-    theta = (data_std) ** 2 / data_mean
-    k = data_mean/theta
-    gamma_density = lambda x: 1 / ( gamma(k) * theta ** k ) * x ** ( k - 1 ) * exp( -x / theta )
+    theta = data_std ** 2 / data_mean
+    k = data_mean / theta
+    gamma_density = lambda x: 1 / (gamma(k) * theta ** k) * x ** (k - 1) * exp(-x / theta)
     return gamma_density
-    
-    
+
+
 def visual_gamma_approx(data):
+    cont = np.array(data["Cont"])
+    max_cont = int(np.max(cont))
+    keys = sorted(data["Hist"].keys())
+    x = np.linspace(0, max_cont + 1)
+    gam = gamma_density_approx(data)
+    y = gam(x)
+    plt.close()
+
+    plt.hist(cont, bins=[s - .5 for s in keys + [keys[-1] + 3]], normed=1, width=1, facecolor='red', label="Data")
+    plt.plot(x, y, '-', color="purple", label="Gamma Distribution")
+
+    plt.xlim(0, max_cont + 1)
+    plt.xlabel("Number of contaminations during trial")
+    plt.ylabel("Portion of Trials")
+    plt.legend(shadow=True, loc=1)
+
+    plt.savefig('hist.png')
+
+
+def visual_poisson_approx(data):
     cont = np.array(data["Cont"])
     max_cont = np.max(cont)
     keys = sorted(data["Hist"].keys())
-    x = np.linspace(0,max_cont)
-    gamma = gamma_density_approx(data)
-    y = gamma(x)
+    # noinspection PyTypeChecker
+    x = np.arange(0, max_cont + 1)
+    lamb = expect(data["p"], data["spread"])
+    dens = lambda k: lamb ** k * exp(-lamb) / factorial(k)
+    y = [dens(n) for n in x]
     plt.close()
 
-    plt.hist(cont, bins=[s-.5 for s in keys+[keys[-1]+3]], normed=1, width=1, facecolor='red', label="Data")
-    plt.plot(x,y,'-',color="purple",label="Gamma Distribution")
+    plt.hist(cont, bins=[s - .5 for s in keys + [keys[-1] + 3]], normed=1, width=1, facecolor='red', label="Data")
+    plt.plot(x, y, '-', color="purple", label="Poisson Distribution")
 
-    plt.xlim(0,max_cont)
+    plt.xlim(0, max_cont + 1)
     plt.xlabel("Number of contaminations during trial")
     plt.ylabel("Portion of Trials")
-    plt.legend(shadow=True,loc=1)
+    plt.legend(shadow=True, loc=1)
 
     plt.savefig('hist.png')
-    
-    
-def visual_expect(prob_spread=exp_spread,P=.75,N=100):
-    x = np.linspace(0, P)
-    y = np.array([batch_simulate(p,prob_spread,N)["Mean"][-1] for p in x])
-    y_exp = expect(x)
+
+
+def visual_expect(prob_spread=exp_spread, p_max=.75, n=100):
+    x = np.linspace(0, p_max)
+    # noinspection PyTypeChecker
+    y = np.array([mean(batch_simulate(p, prob_spread, n)["Cont"]) for p in x])
+    y_exp = np.array([expect(p) for p in x])
     plt.close()
-    plt.plot(x, y, 'x', color="red", label="Mean number of contaminations after " + str(N) + " trials")
+    plt.plot(x, y, 'x', color="red", label="Mean number of contaminations after " + str(n) + " trials")
     plt.plot(x, y_exp, '-', color="purple", label="Expected number of contaminations")
-    plt.legend(shadow=True,loc=2)
+    plt.legend(shadow=True, loc=2)
     plt.xlabel("Probability of Spreading")
     plt.ylabel("Number of contaminations")
-    plt.savefig(str(N) + ' trial graph.png')
-    
-    
-def prob_x(n,s,p,prob_spread=exp_spread):
-    if n == 1:
-        return binomial(3,s) * p ** s * ( 1 - p ) ** ( 3 - s )
-    else:
-        return sum([binomial(3 * r, s) * prob_spread(p,n) ** s * ( 1 - prob_spread(p,n) ) ** ( 3 * r - s ) * prob_x(n-1,r,p,prob_spread) for r in range( int(np.ceil( s / 3 )) , 3 ** ( n - 1 ) + 1 ) ] )
-
-def expect_x(n,p,prob_spread=exp_spread):
-    return sum([s * prob_x(n,s,p,prob_spread) for s in range(3 ** n + 1)])
-
-def expect(p,prob_spread=exp_spread,n_bar=150):
-    return sum([ 3 ** n * p ** ((n ** 2 + n) / 2) for n in range(n_bar)])
-
-
+    plt.savefig(str(n) + ' trial graph.png')
